506bfed762a3444885d5d46f69cb49991c2a1aa4
DEV\nOKeeffe <nicole.okeeffe@velir.com>
2020-09-01 09:02:57 -0400

    Added TestRail links

--- Thread/Scripts/New Test Case/Script1597933936222.groovy
+++ Thread/Scripts/New Test Case/Script1597933936222.groovy
@@ -16,6 +16,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+//newtext
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('http://dev.graybox.velir.com/events')
